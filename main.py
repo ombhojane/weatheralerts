617ae4a1d840ea58cde7577c8e09580f312a0f89
ombhojane <ombhojane05@gmail.com>
2023-07-27 21:51:00 +0530

    added preivous, today, upcoming weather prediction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import requests
+import emoji
+from datetime import datetime, timedelta
+import pandas as pd
 
 # OpenWeatherMap API key (replace 'your_api_key' with your actual API key)
 api_key = 'bb34b4f6362247530f4b2091d0a18a9e'
@@ -19,6 +22,19 @@ class OpenWeatherMapConnection:
         except requests.exceptions.RequestException as e:
             st.error("Error: Unable to connect to the OpenWeatherMap API.")
             return None
+
+# Function to fetch weather data for the previous and next days
+def fetch_weather_data_range(location, days):
+    connection = OpenWeatherMapConnection(api_key)
+    weather_data_list = []
+    for i in range(days):
+        date = datetime.now() + timedelta(days=(i - days // 2))
+        formatted_date = date.strftime("%Y-%m-%d")
+        weather_data = connection.get_weather_data(location)
+        if weather_data:
+            weather_data['date'] = formatted_date
+            weather_data_list.append(weather_data)
+    return weather_data_list
 
 # Main app
 def main():
@@ -44,9 +60,43 @@ def main():
                 st.write(f"Temperature: {weather_data['main']['temp']}°C")
                 st.write(f"Humidity: {weather_data['main']['humidity']}%")
                 st.write(f"Wind Speed: {weather_data['wind']['speed']} m/s")
-                st.write(f"Weather Conditions: {weather_data['weather'][0]['description']}")
+                weather_condition = weather_data['weather'][0]['description']
+                st.write(f"Weather Conditions: {weather_condition}")
+                # Weather Icons
+                if 'clear' in weather_condition.lower():
+                    st.write(emoji.emojize(":sunny:"))
+                elif 'cloud' in weather_condition.lower():
+                    st.write(emoji.emojize(":cloud:"))
+                elif 'rain' in weather_condition.lower():
+                    st.write(emoji.emojize(":cloud_with_rain:"))
+                else:
+                    st.write(emoji.emojize(":question:"))
+
+                st.subheader("Weather Forecast for the Previous 4 Days, Today, and Next 4 Days")
+                weather_data_list = fetch_weather_data_range(location, days=9)
+                df = pd.DataFrame()
+                for i, data in enumerate(weather_data_list):
+                    date = datetime.strptime(data['date'], "%Y-%m-%d")
+                    weekday = date.strftime("%A")
+                    weather = data['weather'][0]['description']
+                    weather_icon = get_weather_icon(weather)
+                    temperature = data['main']['temp']
+                    df[f"{weekday}"] = [weather_icon, temperature]
+
+                st.dataframe(df)
+
             else:
                 st.warning("No weather data found for the given location.")
-    
+
+def get_weather_icon(weather_condition):
+    if 'clear' in weather_condition.lower():
+        return emoji.emojize(":sunny:")
+    elif 'cloud' in weather_condition.lower():
+        return emoji.emojize(":cloud:")
+    elif 'rain' in weather_condition.lower():
+        return emoji.emojize(":cloud_with_rain:")
+    else:
+        return emoji.emojize(":question:")
+
 if __name__ == "__main__":
     main()
